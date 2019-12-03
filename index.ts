8c8935a8ba10ec49a5dcbc42cf92fab3723a139b
HotsumaX <luislaborde@gmail.com>
2019-12-03 14:05:26 -0500

    added some more info on what to do with the data

--- index.ts
+++ index.ts
@@ -3,5 +3,14 @@ import axios from "axios"
 const url = "http://jsonplaceholder.typicode.com/todos/75"
 
 axios.get(url).then(response=>{
-  console.log(response.data)
+  const todo = response.data
+  const ID = todo.ID
+  const title = todo.Title
+  const finished = todo.finished
+
+  console.log(`
+    The Todo with ID: ${ID}
+    has a title of: ${title}
+    is it finished? ${finished}
+  `)
 })
