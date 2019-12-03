3f6117a0ea6c6c5e5adea8a5bb6a72df01e24369
HotsumaX <luislaborde@gmail.com>
2019-12-03 16:18:47 -0500

    corrected the variables

--- index.ts
+++ index.ts
@@ -12,11 +12,15 @@ axios.get(url).then(response=>{
   const todo = response.data as Todo
   const ID = todo.id
   const title = todo.title
-  const finished = todo.completed
+  const completed = todo.completed
 
-  console.log(`
-    The Todo with ID: ${ID}
-    has a title of: ${title}
-    is it finished? ${finished}
-  `)
+  logTodo(id, title, completed)
 })
+
+const logTodo = (id:number, title:string, completed:boolean)=> {
+  console.log(`
+    The Todo with ID: ${id}
+    has a title of: ${title}
+    is it finished? ${completed}
+  `)
+}
